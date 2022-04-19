52652de72c2b1ca454135784d75d9e70c775d243
cliff-rosenberg <100380354+cliff-rosenberg@users.noreply.github.com>
2022-04-19 00:24:37 -0700

    Updated CSS

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,6 +1,6 @@
 // global variables here
 var citySearchEl = document.getElementById("cityName");
-var submitSearch = document.getElementById("btnSearch")
+var submitSearchBtn = document.getElementById("btnSearch")
 
 // turn city name into geolocation coordinates
 async function getGeolocation (cityLookup) {
@@ -44,21 +44,82 @@ async function weatherRequest(lat, lon) {
     };
 };
 
+// function to properly format the date for display
+function formatDate(date) {
+    return [
+      date.getMonth() + 1, 
+      date.getDate(), 
+      date.getFullYear(),
+        ].join('/');
+};
+
 // build out current weather in city after data is returned
-function currentWeatherCity(city, weatherObj) {
-    let cityDate = new Date(weatherObj.current.dt * 1000).toLocaleString();
+function displayWeatherCity(city, weatherObj) {
+    let cityName = city;
+    //console.log(cityFormatted);
+    let cityDate = new Date(weatherObj.current.dt * 1000);
+    let formattedDate = formatDate(cityDate);
+    let iconUrl = weatherObj.current.weather[0].icon;
+    // Attach to second column for display
     let resultsEl = document.getElementById("current-weather");
     resultsEl.innerHTML = "";
     //set up 'box' to hold requested city's weather
     let resultBox = document.createElement('div');
-    resultBox.classList.add('box');
+    resultBox.classList.add('box', 'm-0', 'py-1','has-background-grey-lighter');
     // title is city's name and the date
-    let cityNameEl = document.createElement('h3');
-    cityNameEl.textContent = city + " " + cityDate;
-    let boxContentEl = document.createElement('p');
-    boxContentEl.innerHTML = weatherObj.current.temp + '<br/>';
+    let cityNameEl = document.createElement('span');
+    cityNameEl.classList.add('is-capitalized', 'is-size-2', 'has-text-weight-semibold');
+    cityNameEl.textContent = cityName + " " + "(" + formattedDate + ")";
+    const iconImage = new Image(50, 50);
+    iconImage.src = "http://openweathermap.org/img/wn/" + iconUrl + "@2x.png";
+    cityNameEl.append(iconImage);
+    // Display current weather temp for city searched
+    let boxTemp = document.createElement('p');
+    boxTemp.classList.add('py-3');
+    boxTemp.innerHTML = "Temp: " + weatherObj.current.temp + "&#8457;";
+    // Display current wind speed for city searched
+    let boxWind = document.createElement('p');
+    boxWind.innerHTML = "Wind: " + weatherObj.current.wind_speed + " MPH";
+    // Display current humidity for city searched
+    let boxHumid = document.createElement('p');
+    boxHumid.classList.add('py-3');
+    boxHumid.innerHTML = "Humidity: " + weatherObj.current.humidity + " %";
+    // Display current UV index for city searched
+    let boxUV = document.createElement('p');
+    boxUV.classList.add('pb-3');
+    boxUV.innerHTML = "UV Index: " + weatherObj.current.uvi;
+ 
+    // Build out current weather box
+    resultBox.append(cityNameEl, boxTemp, boxWind, boxHumid, boxUV);
+    resultsEl.append(resultBox);
 
-    resultContentEl.append(resultCard);
+    //build out 5-day forecast
+    var headerForecast = document.createElement('p');
+    headerForecast.classList.add('is-size-3', 'has-text-black', 'px-3', 'py-2')
+    headerForecast.innerHTML = "5-Day Forecast:";
+    resultsEl.append(headerForecast);
+    // cards for daily forecast
+    var cardsContain = document.createElement('div');
+    cardsContain.classList.add('columns', 'm-1');
+    for (let i = 0; i < 5; i++) {
+        let tempDate = new Date(weatherObj.daily[i].dt * 1000);
+        let forecastDate = formatDate(tempDate);
+        let forecastIcon = weatherObj.daily[i].weather[0].icon;
+        let forecastTemp = weatherObj.daily[i].temp.max;
+        let forecastWind = weatherObj.daily[i].wind_speed;
+        let forecastHumid = weatherObj.daily[i].humidity;
+        let cardBaseHTML = `<span id="day-${i}" class="column notification is-info p-1 ml-1 mb-0 has-text-left has-text-white">
+<p class="p-2 is-size-5 has-text-weight-medium">${forecastDate}</p>
+<img src="http://openweathermap.org/img/wn/${forecastIcon}@2x.png" height="50" width="50">
+<p class="pb-2">Temp: ${forecastTemp} &#8457;</p>
+<p class="pb-2">Wind: ${forecastWind} MPH</p>
+<p class="pb-2">Humidity: ${forecastHumid} %</p>
+</span>`;
+        // combine into HTML
+        cardsContain.innerHTML += cardBaseHTML;
+    };
+    // append daily forecast cards
+    resultsEl.append(cardsContain);
 };
 
 // handles the submitted data
@@ -83,12 +144,12 @@ function handleSearchSubmit(event) {
     weatherRequest(reqLat, reqLon).then(data => {
        //console.log(data);
        forecastData = data;
+    }).then(() => {
+        console.log(forecastData);
+        displayWeatherCity(weatherCity, forecastData);
     });
   });// end of async functions
-  console.log(forecastData);
-  currentWeatherCity(weatherCity, forecastData);
-
-};
+};// end of function wrapper
 
 // add event listener for 'search' button
-submitSearch.addEventListener('click', handleSearchSubmit);
+submitSearchBtn.addEventListener('click', handleSearchSubmit);
